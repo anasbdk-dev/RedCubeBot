4d96431f2b5e748579e164f2a3d579f235dacb36
anasbdk-dev <hihyfsgvv@gmail.com>
2026-05-15 02:59:19 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Made with 3ISAM KRISAJ EDDINE",
         // Activity type number (0 = Playing).
         type: 0, 
       },
